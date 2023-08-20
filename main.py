e7868143666232f6f040e77820218af1d7864a30
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-08-20 23:20:31 +0300

    добавлен перезапуск игры

--- main.py
+++ main.py
@@ -105,13 +105,19 @@ class MineSweeper:
                                 1 <= next_button.y <= MineSweeper.COLUMNS and next_button not in queue:
                             queue.append(next_button)
 
+    def reload(self):
+        [child.destroy() for child in self.window.winfo_children()]
+        self.__init__()
+        self.create_widgets()
+        MineSweeper.IS_FIRST_CLICK = True
+
     def create_widgets(self):
 
         menubar = tk.Menu(self.window)
         self.window.config(menu=menubar)
 
         settings_menu = tk.Menu(menubar, tearoff=0)
-        settings_menu.add_command(label='Играть')
+        settings_menu.add_command(label='Играть', command=self.reload)
         settings_menu.add_command(label='Настройки')
         settings_menu.add_command(label='Выход', command=self.window.destroy)
         menubar.add_cascade(label='Файл', menu=settings_menu)
@@ -121,8 +127,14 @@ class MineSweeper:
             for j in range(1, MineSweeper.COLUMNS + 1):
                 btn = self.buttons[i][j]
                 btn.number = count
-                btn.grid(row=i, column=j)
+                btn.grid(row=i, column=j, sticky='NWES')
                 count += 1
+
+        for i in range(1, MineSweeper.ROWS + 1):
+            self.window.rowconfigure(i, weight=1)
+        for i in range(1, MineSweeper.COLUMNS + 1):
+            self.window.columnconfigure(i, weight=1)
+
 
     def open_all_buttons(self):
         for i in range(MineSweeper.ROWS + 2):
